d3a1d45a842159d321e09e1681c2b092a5b2e3fc
wuzhong1598@qq.com <wuzhong1598@qq.com>
2019-01-02 15:34:47 +0800

    add cases code

--- cases/anydoor/src/index.js
+++ cases/anydoor/src/index.js
@@ -1,13 +1,32 @@
-const http = require("http")
-const config = require("./config")
+const http = require("http");
+const config = require("./config");
+const path = require("path");
+const fs = require("fs");
+const promisify = require("util").promisify;
+
+const stat = promisify(fs.stat);
+const readdir = promisify(fs.readdir);
 
 const server = http.createServer((req, res) => {
-    res.statusCode = 200
-    res.setHeader("Content-Type", "text/html")
-    res.write("<html><body><span style='color: red'>Hello world</span></body></html>")
-    res.end()
-})
+  const filePath = path.join(config.root, req.url);
+  try {
+    const stats = stat(filePath);
+    if (stats.isFile()) {
+      res.statusCode = 200;
+      res.setHeader("Content-Type", "text/plain");
+      fs.createReadStream(filePath).pipe(res);
+    } else if (stat.isDirectory()) {
+      //文件夹
+      fs.readdir(filePath, (err, files) => {
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "text/plain");
+        res.end(files.join(","));
+      });
+    }
+  } catch (error) {}
+  
+});
 
 server.listen(config.port, config.host, () => {
-    console.log(`your server is runing http://${config.host}:${config.port}`)
-})
+  console.log(`your server is runing http://${config.host}:${config.port}`);
+});
