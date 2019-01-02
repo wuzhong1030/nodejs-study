af1ba918b856a1ec2e242617ffc4c3da0022b3d0
wuzhong1598@qq.com <wuzhong1598@qq.com>
2019-01-02 16:32:06 +0800

    add example code

--- cases/anydoor/src/index.js
+++ cases/anydoor/src/index.js
@@ -1,30 +1,11 @@
 const http = require("http");
 const config = require("./config");
 const path = require("path");
-const fs = require("fs");
-const promisify = require("util").promisify;
-
-const stat = promisify(fs.stat);
-const readdir = promisify(fs.readdir);
+const route = require("./util/route");
 
 const server = http.createServer((req, res) => {
   const filePath = path.join(config.root, req.url);
-  try {
-    const stats = stat(filePath);
-    if (stats.isFile()) {
-      res.statusCode = 200;
-      res.setHeader("Content-Type", "text/plain");
-      fs.createReadStream(filePath).pipe(res);
-    } else if (stat.isDirectory()) {
-      //文件夹
-      fs.readdir(filePath, (err, files) => {
-        res.statusCode = 200;
-        res.setHeader("Content-Type", "text/plain");
-        res.end(files.join(","));
-      });
-    }
-  } catch (error) {}
-  
+  route(req, res, filePath);
 });
 
 server.listen(config.port, config.host, () => {
---
+++ cases/anydoor/src/util/route.js
@@ -0,0 +1,41 @@
+const fs = require("fs");
+const path = require("path")
+const promisify = require("util").promisify;
+
+const stat = promisify(fs.stat);
+const readdir = promisify(fs.readdir);
+const config = require("../config")
+
+const handleBars = require("handlebars")
+
+const source = fs.readFileSync(path.join(__dirname, "../template/dir.tpl"))
+const template = handleBars.compile(source.toString())
+
+module.exports = async function(req, res, filePath) {
+  try {
+    const stats = await stat(filePath);
+
+    if (stats.isFile()) {
+      res.statusCode = 200;
+      res.setHeader("Content-Type", "text/plain");
+      fs.createReadStream(filePath).pipe(res);
+    } else if (stats.isDirectory()) {
+      //文件夹
+      const files = await readdir(filePath);
+      res.statusCode = 200;
+      res.setHeader("Content-Type", "text/html");
+      const dir = path.relative(config.root, filePath)
+      const data = {
+        files,
+        dir: dir ? `/${dir}` : '',
+        title: path.basename(filePath)
+      }
+      res.end(template(data));
+    }
+  } catch (error) {
+    console.error(error);
+    res.statusCode = 500;
+    res.setHeader("Content-Type", "text/plain");
+    res.end(`error: \n ${error.toString()}`);
+  }
+};
